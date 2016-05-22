4f953902ffec2ad811c559c55f64ea32e806e2a0
Josh Birdwell <jbirdwell34@gmail.com>
2016-05-22 09:45:03 -0500

    update

--- scripts/App.js
+++ scripts/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import FuzzySearch from './FuzzySearch';
+import { Parallax } from 'react-parallax';
+
 const seating = require('./seating');
 
 export default class App extends Component {
@@ -20,6 +22,10 @@ export default class App extends Component {
 
     return (
         <section className="content">
+          <div className="header-image">
+              <div className="background"/>
+              <div className="logo"/>
+          </div>
           <h2 className="heading">Wedding Seating</h2>
           <FuzzySearch className="row" showResult={this.showResult}/>
           <h4 className="row result">{result}</h4>
